e7d2ab9f8ede4a66b8201716a3b99aceffc50f2c
andrzej.skowron <>
2023-01-25 17:48:41 +0100

    fixes 2

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -21,9 +21,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT( \
 //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-    KC_GESC,   KC_Q,   KC_D,    KC_R,    KC_W,    KC_B,                     KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN,    KC_MINS, \
+    KC_GRV,   KC_Q,   KC_D,    KC_R,    KC_W,    KC_B,                     KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN,    KC_MINS, \
 //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-    KC_LCTRL, KC_A,   KC_S,    KC_H,    KC_T,    KC_G,                     KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT, \
+    KC_LCTL, KC_A,   KC_S,    KC_H,    KC_T,    KC_G,                     KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT, \
 //,-----------------------------------------------------.                    ,-----------------------------------------------------.
     KC_LSFT,  KC_Z,   KC_X,    KC_M,    KC_C,    KC_V,                      KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
 //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -33,9 +33,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_LOWER] = LAYOUT( \
 //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-    KC_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_UP,    KC_9,    KC_0,    KC_EQUAL, \
+    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_UP,    KC_9,    KC_0,    KC_EQUAL, \
 //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-    KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_LEFT, KC_DOWN, KC_RIGHT, KC_RPRN, _______, \
+    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_LEFT, KC_DOWN, KC_RIGHT, KC_RPRN, _______, \
 //,-----------------------------------------------------.                    ,-----------------------------------------------------.
     _______, _______, _______, _______, _______, _______,                    XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
 //,-----------------------------------------------------.                    ,-----------------------------------------------------.
